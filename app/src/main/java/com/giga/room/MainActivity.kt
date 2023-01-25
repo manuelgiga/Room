a0d57259c6acb491319f28afbc49f109f7ae9bef
manuel hernandez <wilson.hernandez@gigafibra.co>
2023-01-25 09:52:05 -0500

    ahora si

--- app/src/main/java/com/giga/room/MainActivity.kt
+++ app/src/main/java/com/giga/room/MainActivity.kt
@@ -3,6 +3,7 @@ package com.giga.room
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.room.Room
 import kotlinx.coroutines.*
@@ -21,13 +22,22 @@ class MainActivity : AppCompatActivity() {
         ).build()
 
         val noteDao = db.noteDao()
-        val note = Note(0, "title", "content")
+        val note = Note(0, "el bosque de sauces", "trata de animales")
 
         GlobalScope.launch(Dispatchers.IO) {
             noteDao.insert(note)
             val allNotes = noteDao.getAll()
             withContext(Dispatchers.Main) {
-                Log.d("Notes", allNotes.toString())
+                var extension = allNotes.size
+                if (extension<5){
+                    Toast.makeText(this@MainActivity, "medida: $extension", Toast.LENGTH_SHORT).show()
+                    Log.d("Notes", allNotes.toString())
+                }else{
+                    Toast.makeText(this@MainActivity, "Ya no puedo mas", Toast.LENGTH_SHORT).show()
+                    Log.d("Notes", allNotes.toString())
+                }
+
+
             }
         }
 
